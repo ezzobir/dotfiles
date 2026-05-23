0f39b0bdf347ede7609127ad5657bc6b81836cc6
ezzobir <ezzobir_bezziou@protonmail.com>
2026-05-23 11:03:04 +0100

    add window rules for hyprland

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -408,3 +408,25 @@ hl.window_rule({
     move  = "20 monitor_h-120",
     float = true,
 })
+
+-- windowrule
+hl.window_rule({
+    name  = "firefox-on-ws1",
+    match = { class = "^(firefox)$" },
+    -- workspace = "1 silent"
+    workspace = "1"
+})
+
+hl.window_rule({
+    name  = "kitty-on-ws2",
+    match = { class = "^(kitty)$" },
+    -- workspace = "1 silent"
+    workspace = "2"
+})
+
+hl.window_rule({
+    name  = "telegram-on-ws3",
+    match = { class = "^(telegram-desktop)$" },
+    -- workspace = "1 silent"
+    workspace = "3"
+})
