0e343cdc0d8b44ba755ed49999c35f9119ec072d
ezzobir <ezzobir_bezziou@protonmail.com>
2026-06-04 10:47:52 +0100

    add some changes to fish configs

--- .config/emacs/emacs-custom.el
+++ .config/emacs/emacs-custom.el
@@ -4,7 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(anzu avy change-inner embrace fish-mode flx hyprlang-ts-mode
+	  markdown-mode multiple-cursors typst-ts-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
