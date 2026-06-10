40bbc5f28a0ee516b4ebbe7c6de09eb354a2ef33
ezzobir <ezzobir_bezziou@protonmail.com>
2026-06-10 10:08:51 +0100

    add some changes to fish and emacs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -40,6 +40,21 @@ set -U fish_user_paths /home/ezzobir/efs/repos/github.com/configs/scripts/rofi $
 # # -----------------------------------------------------
 # # END HELIX MODE
 
+# # BEGIN VI_EMACS MODE
+# # -----------------------------------------------------
+# function fish_user_key_bindings
+#     # Execute this once per mode that emacs bindings should be used in
+#     fish_default_key_bindings -M insert
+
+#     # Then execute the vi-bindings so they take precedence when there's a conflict.
+#     # Without --no-erase fish_vi_key_bindings will default to
+#     # resetting all bindings.
+#     # The argument specifies the initial mode (insert, "default" or visual).
+#     fish_vi_key_bindings --no-erase insert
+# end
+# # -----------------------------------------------------
+# # END VI_EMACS MODE
+
 # BEGIN HELIX_EMACS MODE
 # -----------------------------------------------------
 function fish_user_key_bindings
