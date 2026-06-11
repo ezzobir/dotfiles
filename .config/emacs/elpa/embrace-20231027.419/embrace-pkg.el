37310a0a3cf29c0f24b73d3bba0adba1d021af5b
ezzobir <ezzobir_bezziou@protonmail.com>
2026-06-11 11:26:34 +0100

    add some changes to fish and emacs

---
+++ .config/emacs/elpa/embrace-20231027.419/embrace-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "embrace" "20231027.419"
+  "Add/Change/Delete pairs based on `expand-region'."
+  '((cl-lib        "0.5")
+    (expand-region "0.10.0"))
+  :url "https://github.com/cute-jumper/embrace.el"
+  :commit "c7e748603151d7d91c237fd2d9cdf56e9f3b1ea8"
+  :revdesc "c7e748603151"
+  :keywords '("extensions")
+  :authors '(("Junpeng Qiu" . "qjpchmail@gmail.com"))
+  :maintainers '(("Junpeng Qiu" . "qjpchmail@gmail.com")))
